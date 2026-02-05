0e0831880b9e5f4305e9c8275376b66232e53556
cminnstockdale-droid <c.minn.stockdale@gmail.com>
2026-02-05 22:32:04 +0000

    Part one of homework assignment for week 5 is completed

---
+++ homework5_<catherine_stockdale>.py
@@ -0,0 +1,16 @@
+#Step 1: Create a List that Contains 6 Items
+shoes = ["Sambas", "Birkenstocks", "Cowgirl boots", "Hokas", "Chacos", "Vans Slip-ons"]
+#Step 2: Print the items to the output console
+print(shoes)
+#Step 3: Print the first two items in the list using a slice.  The output message should be: The first two items in the list are: item1, item2
+print(f"The first two items in the list are: {shoes[0]}, {shoes[1]}")
+#Step 4: Print the middle two items in the list using a slice.  The output message should be: The middle two items in the list are: item3, item4
+print(f"The middle two items in the list are: {shoes[2]}, {shoes[3]}")
+#Step 5: Print the first and last items in the list using indexes.  The output message should be: The first and last items in the list are: item1, item6
+print(f"The first and last items in the list are: {shoes[0]}, {shoes[5]}")
+
+#Transition to Tuple Exercise for Restaurant Menu Items 
+#Step 1 Tuple Exercise: 1.	A restaurant only offers five basic foods on its menu.  Decide on these five foods and store them in a tuple.
+#Step 2: Print each item on the menu using a for loop.
+#Step 3: The restaurant has updated its menu, replacing two of the items with different foods.  Copy the tuple replacing two of the foods on the menu
+#Step 4: Print each item on the revised menu using a loop.
